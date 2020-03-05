feb6993d893626cb3aac549f4a14dd50136bdc91
hananzainal <hananzainal26@gmail.com>
2020-03-05 14:52:22 +0800

    Working App

---
+++ src/java/my/presentation/MessageView.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.presentation;
+
+import boundary.MessageFacade;
+import entities.Message;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author LightozRyuk
+ */
+@Named(value = "MessageView")
+@RequestScoped
+public class MessageView {
+
+    @EJB
+    private MessageFacade messageFacade;
+
+    private Message message;
+    /**
+     * Creates a new instance of MessageView
+     */
+    public MessageView() {
+        this.message = new Message();
+    }
+    
+    public Message getMessage() {
+        return message;
+    }
+    
+    public int getNumberOfMessages() {
+        return messageFacade.findAll().size();
+    }
+    
+    public String postMessage() {
+        this.messageFacade.create(message);
+        return "theend";
+    }
+    
+    public List viewAllMessages() {
+        return messageFacade.findAll();
+    }
+}
